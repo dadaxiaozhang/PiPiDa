210d9b1e7d98d0fbc13fed6bd1241a458ae182bf
季航 <836042860@qq.com>
2017-10-27 17:15:34 +0800

    初始化工程

---
+++ src/com/lanou3g/daomain/Server.java
@@ -0,0 +1,29 @@
+package com.lanou3g.daomain;
+
+import java.util.List;
+
+/**
+ * Created by dllo on 17/10/27.
+ */
+public class Server {
+    private int id;
+    private String sname;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: dllo
+  Date: 17/10/27
+  Time: 上午9:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>游戏</title>
+  </head>
+  <body>
+  <h1>选择游戏大蛆</h1>
+  <a href="playGame.action">选择游戏大蛆</a>
+  </body>
+</html>
